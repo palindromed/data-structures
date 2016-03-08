aebf5b634a166f542e6e991993a0aa9a4292c0f5
Michael Stokley <michael@debianT430.stokley>
2016-03-08 15:23:00 -0800

    added docstrings

--- stack.py
+++ stack.py
@@ -2,11 +2,14 @@ from linked_list import LinkedList, Node
 
 
 class Stack(object):
+    """Stack data type is composed using linked list data type"""
     def __init__(self, iterable=[]):
         self.ll = LinkedList(iterable)
 
     def push(self, val):
+        """Add val to top of stack"""
         return self.ll.insert(val)
 
     def pop(self):
+        """Remove and return first element of stack"""
         return self.ll.pop()
