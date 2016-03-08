b7a0675e04d1a8569d4f366e97af8c141a12e2cd
Michael Stokley <michael@debianT430.stokley>
2016-03-07 16:18:54 -0800

    added insert() tests

--- test_linked_list.py
+++ test_linked_list.py
@@ -1,4 +1,16 @@
+
 def test_init_empty_list():
     from linked_list import LinkedList
     new_list = LinkedList()
     assert new_list.head_node.value == None
+
+def test_insert():
+    from linked_list import LinkedList
+    new_list = LinkedList()
+    new_list.insert(5)
+    assert new_list.head_node.value == 5
+    new_list.insert(10)
+    assert new_list.head_node.value == 10
+    assert new_list.head_node.next.value == 5
+    assert new_list.head_node.next.next.value == None
+
