b2da7951097492d061a810586100e1d80fcb84ec
Hannah Krager <hannahkrager@gmail.com>
2016-03-13 10:22:24 -0700

    parametrize test

--- test_proper_parenthetics.py
+++ test_proper_parenthetics.py
@@ -1,16 +1,12 @@
-def test_open_string():
+import pytest
+
+
+TEST_OPTIONS = [("()()(DJKLSJDF", 1),
+                ("()()(DJKLSJDF)(((())))", 0),
+                ("()())DJKLSJDF(", -1)]
+
+
+@pytest.mark.parametrize("test_input, expected", TEST_OPTIONS)
+def test_proper_parens(test_input, expected):
     from proper_parenthetics import proper_parens
-    test_input = "()()(DJKLSJDF"
-    assert proper_parens(test_input) == 1
-
-
-def test_balanced_string():
-    from proper_parenthetics import proper_parens
-    test_input = "()()(DJKLSJDF)(((())))"
-    assert proper_parens(test_input) == 0
-
-
-def test_broken_string():
-    from proper_parenthetics import proper_parens
-    test_input = "()())DJKLSJDF("
-    assert proper_parens(test_input) == -1
+    assert proper_parens(test_input) == expected
