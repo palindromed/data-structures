67e0c7d6fb76081daf71aa062bf1a5f932898a67
Michael Stokley <michael@debianT430.stokley>
2016-03-15 19:35:55 -0700

    bug fixes on heapify, added pop()

--- src/binheap.py
+++ src/binheap.py
@@ -1,3 +1,6 @@
+import math
+
+
 class BinHeap(object):
     def __init__(self, init=[]):
         self._container = init
@@ -6,17 +9,33 @@ class BinHeap(object):
             self._heapify(1)
 
     def _heapify(self, index):
+        # indices
         left = 2 * index
         right = 2 * index + 1
         largest = index
 
-        if left <= len(self._container) and self._container[left] > self._container[largest]:
+        if left <= (len(self._container) - 1) and self._container[left] > self._container[largest]:
             largest = left
-        if right <= len(self._container) and self._container[right] > self._container[largest]:
+        if right <= (len(self._container) - 1) and self._container[right] > self._container[largest]:
             largest = right
-
         if largest != index:
             this_node_value = self._container[index]
             self._container[index] = self._container[largest]
             self._container[largest] = this_node_value
             self._heapify(largest)
+
+    def pop(self):
+        self._container[1] = self._container.pop()
+        self._heapify(1)
+
+
+    def push(self, value):
+        self._container.append(value)
+        #  compare this node with parent
+        # [None, 1, 2, 3]
+        child = (len(self._container) - 1)
+        parent = math.floor(child / 2)
+        while (self._container[child] > self._container[parent] and child > 1):
+            # swap parent and child
+            # move up the chain towards the root
+            pass
--- src/test_binheap.py
+++ src/test_binheap.py
@@ -3,16 +3,37 @@
 import pytest
 
 
-@pytest.fixture
-def create_heap_for_tests():
-    from binheap import BinHeap
-    sample_list = [None, 16, 14, 15, 9, 7, 6, 5, 1, 2, 3]
-    bh = BinHeap(sample_list)
+# @pytest.fixture
+# def create_heap_for_tests(scope="function"):
+#     from binheap import BinHeap
+#     sample_list = [16, 14, 15, 9, 7, 6, 5, 1, 2, 3]
+#     bh = BinHeap(sample_list)
 
 
 def test_push():
+    from binheap import BinHeap
+    sample_list = [16, 14, 15, 9, 7, 6, 5, 1, 2, 3]
+    bh = BinHeap(sample_list)
     bh.push(4)
     assert bh._container == sample_list.append(4)
 
 
-def test_push_larger_value():
+# def test_push_larger_value():
+
+
+def test_pop_empty_heap():
+    from binheap import BinHeap
+    bh = BinHeap()
+    with pytest.raises(IndexError):
+        bh.pop()
+
+
+def test_pop():
+    from binheap import BinHeap
+    bh = BinHeap([1, 2, 3])
+    bh.pop()
+    # [3, 2, 1]
+    # [1, 2, 3]
+    # [1, 2]
+    # [2, 1]
+    assert bh._container == [None, 2, 1]
