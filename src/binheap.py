82b03e28d32d9b38b75d358e3dcc1b47ff227445
Michael Stokley <michael@debianT430.stokley>
2016-03-16 09:59:15 -0700

    tiny cleanup, added new tests

--- src/binheap.py
+++ src/binheap.py
@@ -1,39 +1,39 @@
+
+# -*- coding: utf-8 -*-
 import math
 
 
 class BinHeap(object):
     def __init__(self, init=[]):
+        # TODO: test if works when root, childen do not violate heap property
         self._container = init
         self._container.insert(0, None)
         if init != []:
             self._heapify(1)
 
     def _heapify(self, index):
-        # indices
-        left = 2 * index
-        right = 2 * index + 1
+        left, right = 2 * index, 2 * index + 1
         largest = index
+        length = len(self._container) - 1
 
-        if left <= (len(self._container) - 1) and self._container[left] > self._container[largest]:
+        if left <= length and self._container[left] > self._container[largest]:
             largest = left
-        if right <= (len(self._container) - 1) and self._container[right] > self._container[largest]:
+        if right <= length and self._container[right] > self._container[largest]:
             largest = right
         if largest != index:
             this_node_value = self._container[index]
             self._container[index] = self._container[largest]
             self._container[largest] = this_node_value
+            # TODO: rewrite as while loop
             self._heapify(largest)
 
     def pop(self):
         self._container[1] = self._container.pop()
         self._heapify(1)
 
-
     def push(self, value):
         self._container.append(value)
-        #  compare this node with parent
-        # [None, 1, 2, 3]
-        child = (len(self._container) - 1)
+        child = len(self._container) - 1
         parent = math.floor(child / 2)
         while (self._container[child] > self._container[parent] and child > 1):
             # swap parent and child
--- src/test_binheap.py
+++ src/test_binheap.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 
 import pytest
@@ -8,6 +9,12 @@ import pytest
 #     from binheap import BinHeap
 #     sample_list = [16, 14, 15, 9, 7, 6, 5, 1, 2, 3]
 #     bh = BinHeap(sample_list)
+
+ITERABLES = [
+    ([1, 2, 3], [None, 3, 2, 1]),
+    ([3, 2, 1], [None, 3, 2, 1]),
+    ([3, 2, 1, 4], [None, 4, 3, 1, 2])
+]
 
 
 def test_push():
@@ -37,3 +44,16 @@ def test_pop():
     # [1, 2]
     # [2, 1]
     assert bh._container == [None, 2, 1]
+
+
+def test_init():
+    from binheap import BinHeap
+    bh = BinHeap()
+    assert bh._container == [None]
+
+
+@pytest.mark.parametrize(("iterable", "expected"), ITERABLES)
+def test_init_from_iter(iterable, expected):
+    from binheap import BinHeap
+    bh = BinHeap(iterable)
+    assert bh._container == expected
