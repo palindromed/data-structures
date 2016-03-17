86f8fed8e836c9e16e567cbc12d048e7c6bd6e17
Michael Stokley <michael@debianT430.stokley>
2016-03-17 14:38:49 -0700

    methods have skeletons

--- src/priorityq.py
+++ src/priorityq.py
@@ -1,2 +1,22 @@
 class PriorityQueue(object):
-    pass
+
+
+    def __init__(self):
+        """Create a priority queue."""
+        self._container = []
+
+    def insert(self, item):
+        """Insert item into priority queue."""
+        self._container.
+
+    def pop(self):
+        """Remove and return highest priority item in priority queue."""
+
+    def peek(self):
+        """Return next (highest priority) item in priority queue without removing it."""
+
+    def _sort_down(self, index):
+        # helper method to maintain heap property after popping
+
+    def _sort_up(self, index):
+        # helper method to maintain heap property after inserting
